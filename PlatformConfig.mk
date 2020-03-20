e4a6a66f47e1339a2cd1ce217994a09e1838a46a
sjllls <2012ji123@gmail.com>
2020-03-21 00:43:23 +0800

    PlatformConfig: Define absolute path of kernel

--- PlatformConfig.mk
+++ PlatformConfig.mk
@@ -18,6 +18,7 @@ PRODUCT_PLATFORM_SOD := true
 
 TARGET_BOARD_PLATFORM := msm8996
 KERNEL_PATH := kernel/sony/msm-4.4
+TARGET_KERNEL_SOURCE := kernel/sony/msm-4.4/kernel
 
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
